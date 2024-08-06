e5ec1701f7671d1de07a79a105545fcdf4f85d6b
Sumit Chhetri <sumitchhetri277@gmail.com>
2024-08-06 08:40:06 +0545

    Pilandrome

---
+++ po.js
@@ -0,0 +1,33 @@
+const input = document.getElementById("text-input");
+const button = document.getElementById("check-btn");
+const result = document.getElementById("result");
+
+button.addEventListener("click", () => {
+    const inputText = input.value;
+
+    if (inputText === "") {
+        alert("Please input a word that can be palindrome or not");
+        rerurn;
+        //unnecessary to add but the project wants to include.
+    }
+
+    //Simple logic by adding a function
+    function ispalindrome(str) {
+        //[/w_/g,''] another one of meta characters
+        const alphaneumaricalOnly = str.replace(/[A^ZA-z0-9\s]/g, '').toLoweCase();
+
+        const alphaneumaricalOnlynoSpace = alphaneumaricalOnly.replace(/\s/g, '');
+        //Reversing the cleaned string without space
+        const reversed = alphaneumaricalOnlynoSpace.split('').reverse().join('');
+        //Comparing cleaned string without any spaces with reversed string
+        return alphaneumaricalOnlynoSpace === reversed;
+    }
+
+    //Checking of the input if palindrome and dsiplay accordingly
+    if (ispalindrome(inputText)) {
+        result.innerText = `${inputText} is a palindrome`;
+    }
+    else {
+        result.innerText = `${inputText} is not a palindrome`;
+    }
+})
